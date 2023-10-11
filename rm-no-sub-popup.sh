70bcab7be60d045a4e8e6228ff678ec115cddd89
guoqiao <guoqiao@pve>
2023-10-12 09:42:54 +1300

    add rm-no-sub-popup.sh

---
+++ rm-no-sub-popup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# every time when you login to pve webui, there will be a pop up for no valid sub.
+# backup this file, edit it to disable it
+
+sudo vim /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
